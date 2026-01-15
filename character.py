46f1232ab6d9e9a532a942883573f62a6955d11d
Rowan Murdock <rowanmurdock@gmail.com>
2026-01-14 22:09:10 -0700

    creating basic classes needed for world generation

---
+++ character.py
@@ -0,0 +1,7 @@
+class Character:
+    def __init__(self, name, birthdate, deathdate, traits, role):
+        self.name = name
+        self.birthdate = birthdate
+        self.deathdate = deathdate
+        self.traits = traits
+        self.role = role
---
+++ faction.py
@@ -0,0 +1,11 @@
+class Faction:
+    def __init__(self, name, culture, religion, leader, government):
+        self.name = name
+        self.culture = culture
+        self.religion = religion
+        self.leader = leader
+        self.government = government
+    
+        
+
+  
