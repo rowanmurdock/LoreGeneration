6ed15866ee1e3de3da3965ff68b87be6753bbe61
Rowan Murdock <rowanmurdock@gmail.com>
2026-01-16 19:24:54 -0700

    fixing some main bugs and testing character objects

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-import names
 from names import *
 import random
 from names import ROLE_3_TITLES
 from names import ROLE_2_TITLES
 from names import ROLE_1_TITLES
 from names import ROLE_0_TITLES
-from traits import TRAITS
+from traits import CHARACTER_TRAITS
 from character import Character
 
 from names import NAMES
@@ -13,10 +12,10 @@ from names import NAMES
 def generate_random_character():
     name = random.choice(NAMES)
     birthdate = random.randint(0, 80)
-    deathdate = None
-    cause_of_death = None
+    
 
-    traits = random.sample(TRAITS, random.randint(1, 3))
+
+    traits = random.sample(list(CHARACTER_TRAITS.keys()), random.randint(1, 3))
     role_rank = random.randint(0, 3)
     match role_rank:
         case 3:
@@ -31,9 +30,7 @@ def generate_random_character():
     return Character(
         name=name,
         birthdate=birthdate,
-        deathdate=deathdate,
         traits=traits,
-        cause_of_death=cause_of_death,
         role_title=role_title,
         role_rank=role_rank
     )
